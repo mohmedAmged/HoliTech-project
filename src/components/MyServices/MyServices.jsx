05b7f2b4cd5f1509c27ba633e35da2a2e3f66134
Mohamed Amged <mohamedamged55555@gmail.com>
2024-02-26 19:11:38 +0200

    initialize services section

---
+++ src/components/MyServices/MyServices.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './myServices.css'
+import { NavLink } from 'react-router-dom'
+export default function MyServices({ servicesArray }) {
+    return (
+        <div className='myServices__handler sec_padding'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 col-lg-4 col-xl-4">
+                        <div className="myServices__title">
+                            <span>what we do</span>
+                            <h2>we work performed for client happy.</h2>
+                            <NavLink to={'/services'} className='nav-link'>
+                                <div className="get__touch__btn hover__effect">
+                                    view all services
+                                </div>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className="col-md-12 col-lg-8 col-xl-8">
+                        <div className="row g-4">
+                            {
+                                servicesArray?.map((item, index) => (
+                                    <div key={index} className="col-md-6 col-lg-6 col-sm-6 col-xl-6">
+                                        <div className="single__card__service">
+                                            <span>{item?.num}</span>
+                                            <div className="service__icon">
+                                                <i>
+                                                    <img src={item?.icon} alt="icon" />
+                                                </i>
+                                            </div>
+                                            <div className="service__content">
+                                                <h4>
+                                                    {item?.title}
+                                                </h4>
+                                                <p>
+                                                    {item?.description}
+                                                </p>
+                                                <NavLink className="nav-link service__card__btn" to={'/service-detail'}>
+                                                    read more <i className="bi bi-arrow-right-circle"></i>
+                                                </NavLink>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/myMainHome/MyMainHome.jsx
+++ src/pages/myMainHome/MyMainHome.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import './myMainHome.css'
 import HomeHeroSlider from '../../components/homeHeroSlider/HomeHeroSlider'
+import MyServices from '../../components/MyServices/MyServices'
+import webIcon from '../../assets/svgIcons/web.png'
+import mopileIcon from '../../assets/svgIcons/mobile-development.png'
+import UiUx from '../../assets/svgIcons/ui-ux.png'
+import graphicIcon from '../../assets/svgIcons/illustration.png'
 export default function MyMainHome() {
+    const servicesArray = [
+        {
+            num: "01",
+            title: "Web Development",
+            description: "Fusce ornare mauris nisi, id fringilla turpis vehicula justo lectus, ultricies nec sem",
+            icon: webIcon
+        },
+        {
+            num: "02",
+            title: "App Development",
+            description: "Fusce ornare mauris nisi, id fringilla turpis vehicula justo lectus, ultricies nec sem",
+            icon: mopileIcon
+        },
+        {
+            num: "03",
+            title: "UI/UX Design",
+            description: "Fusce ornare mauris nisi, id fringilla turpis vehicula justo lectus, ultricies nec sem",
+            icon: UiUx
+        },
+        {
+            num: "04",
+            title: "Graphic Design",
+            description: "Fusce ornare mauris nisi, id fringilla turpis vehicula justo lectus, ultricies nec sem",
+            icon: graphicIcon
+        },
+    ];
     return (
         <>
             <HomeHeroSlider />
+            <MyServices servicesArray={servicesArray}/>
         </>
     )
 }
